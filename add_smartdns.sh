3cc89c8657833e11b43e25d2f0ccae4823827840
soda1107 <b5282521@live.cn>
2025-07-28 13:49:44 +0800

    更新 add_smartdns.sh，添加 InfinityDuck 克隆深度限制，并更新自定义源配置

--- add_smartdns.sh
+++ add_smartdns.sh
@@ -27,6 +27,7 @@ rm $WORKINGDIR/${LUCIBRANCH}.zip
 
 #WORKINGDIR="`pwd`/packages/new/“
 #mkdir $WORKINGDIR -p
-git clone https://github.com/JohnsonRan/InfinityDuck package/new/InfinityDuck
+git clone https://$github/JohnsonRan/InfinityDuck package/new/InfinityDuck --depth=1
+echo "src/gz infsubs https://opkg.ihtw.moe/openwrt-24.10/$arch/InfinitySubstance" >>files/etc/opkg/customfeeds.conf
 
 
