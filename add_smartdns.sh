b99229069491e3eaf53a25361ff899b69218cfba
soda1107 <b5282521@live.cn>
2025-08-02 23:07:33 +0800

    fix

--- add_smartdns.sh
+++ add_smartdns.sh
@@ -5,6 +5,7 @@ cd ./wrt/
 ./scripts/feeds update -a
 MAKEFILE_PATH="feeds/packages/net/smartdns/Makefile"
 WORKINGDIR="`pwd`/feeds/packages/net/smartdns"
+MAKEFILE=$(find wrt/feeds/packages/net/smartdns/ -name Makefile | head -n1)
 mkdir $WORKINGDIR -p
 rm $WORKINGDIR/* -fr
 #mkdir $WORKINGDIR/plugin -p
@@ -23,7 +24,18 @@ sed -i 's#^\s*include \.\./\.\./lang/rust/rust-package.mk#include $(TOPDIR)/feed
 sed -i '/define Build\/Compile\/smartdns-ui/,/endef/ {
     /^\s*RUSTFLAGS=/ s/\"$/ -C prefer-dynamic -C link-arg=-lc -C link-arg=-lm\"/
 }' "$MAKEFILE_PATH"
-sed -i '/^  DEPENDS:=+smartdns/ s/$/ +libc:0 +libm:0/' "$MAKEFILE_PATH"
+#sed -i '/^  DEPENDS:=+smartdns/ s/$/ +libc:0 +libm:0/' "$MAKEFILE_PATH"
+sed -i '/Build\/Compile\/smartdns-ui/,/endef/ {
+      s|make -C .*||g
+      a \
+export PATH=\$\$PATH:\$(CARGO_HOME)/bin ; \\
+export CC=\$(TARGET_CC) ; \\
+export CXX=\$(TARGET_CXX) ; \\
+export AR=\$(TARGET_AR) ; \\
+export RUSTFLAGS="-C linker=\$(TARGET_CC)" ; \\
+cargo build --release --target aarch64-unknown-linux-musl \\
+  --manifest-path=\$(PKG_BUILD_DIR)/plugin/smartdns-ui/Cargo.toml
+    }' "$MAKEFILE"
 echo "已修改 Makefile，以下是 smartdns 相关部分的新内容："
 sed -n '/define Package\/smartdns/,/endef/p' "$MAKEFILE_PATH"
 
