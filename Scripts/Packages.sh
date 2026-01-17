a35fde96cd78cb6276649c18e4b1397085eb568c
soda1107 <b5282521@live.cn>
2026-01-17 21:50:22 +0800

    r

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -125,8 +125,8 @@ UPDATE_VERSION() {
 sed -i 's/+xray-core//' luci-app-passwall2/Makefile
 
 #删除官方的默认插件
-rm -rf ../feeds/luci/applications/luci-app-{passwall*,mosdns,dockerman,dae*,bypass*}
-rm -rf ../feeds/packages/net/{v2ray-geodata,dae*}
+rm -rf ../feeds/luci/applications/luci-app-{passwall*,mosdns,dockerman,bypass*}
+rm -rf ../feeds/packages/net/{v2ray-geodata}
 
 #更新golang为最新版
 rm -rf ../feeds/packages/lang/golang
@@ -139,10 +139,10 @@ cp -r $GITHUB_WORKSPACE/package/* ./
 sed -i 's/mkdir \$(PKG_BUILD_DIR)\/\$(ARCH)/mkdir -p \$(PKG_BUILD_DIR)\/\$(ARCH)/g' ../feeds/packages/utils/coremark/Makefile
 
 #修改字体
-argon_css_file=$(find ./luci-theme-argon/ -type f -name "cascade.css")
-sed -i "/^.main .main-left .nav li a {/,/^}/ { /font-weight: bolder/d }" $argon_css_file
-sed -i '/^\[data-page="admin-system-opkg"\] #maincontent>.container {/,/}/ s/font-weight: 600;/font-weight: normal;/' $argon_css_file
+#argon_css_file=$(find ./luci-theme-argon/ -type f -name "cascade.css")
+#sed -i "/^.main .main-left .nav li a {/,/^}/ { /font-weight: bolder/d }" $argon_css_file
+#sed -i '/^\[data-page="admin-system-opkg"\] #maincontent>.container {/,/}/ s/font-weight: 600;/font-weight: normal;/' $argon_css_file
 
 #修复daed/Makefile
-rm -rf luci-app-daed/daed/Makefile && cp -r $GITHUB_WORKSPACE/patches/daed/Makefile luci-app-daed/daed/
-cat luci-app-daed/daed/Makefile
+#rm -rf luci-app-daed/daed/Makefile && cp -r $GITHUB_WORKSPACE/patches/daed/Makefile luci-app-daed/daed/
+#cat luci-app-daed/daed/Makefile
