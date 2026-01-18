dfb9ef1424db50acb7f78059c832c3e0e29063d8
soda1107 <b5282521@live.cn>
2026-01-18 11:25:04 +0800

    a

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -68,7 +68,7 @@ UPDATE_PACKAGE "netspeedtest" "sirpdboy/luci-app-netspeedtest" "master" "" "home
 UPDATE_PACKAGE "partexp" "sirpdboy/luci-app-partexp" "main"
 #UPDATE_PACKAGE "qbittorrent" "sbwml/luci-app-qbittorrent" "master" "" "qt6base qt6tools rblibtorrent"
 #UPDATE_PACKAGE "qmodem" "FUjr/QModem" "main"
-UPDATE_PACKAGE "viking" "VIKINGYFY/packages" "main" "" "luci-app-timewol luci-app-wolplus"
+#UPDATE_PACKAGE "viking" "VIKINGYFY/packages" "main" "" "luci-app-timewol luci-app-wolplus"
 #UPDATE_PACKAGE "vnt" "lmq8267/luci-app-vnt" "main"
 #UPDATE_PACKAGE "adguardhome" "sirpdboy/luci-app-adguardhome" "main" "" "luci-app-adguardhome"
 UPDATE_PACKAGE "luci-app-daed" "QiuSimons/luci-app-daed" "master"
