fd39f3e4cf47975dbd3ab7be9437920423bec243
Luke xiao <39824220+soda1107@users.noreply.github.com>
2026-04-30 14:53:23 +0800

    更新 Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -77,7 +77,7 @@ UPDATE_PACKAGE "vnt" "lmq8267/luci-app-vnt" "main"
 
 
 
-UPDATE_PACKAGE "luci-app-daed" "QiuSimons/luci-app-daed" "kix"
+#UPDATE_PACKAGE "luci-app-daed" "QiuSimons/luci-app-daed" "kix"
 UPDATE_PACKAGE "luci-app-pushbot" "zzsj0928/luci-app-pushbot" "master"
 UPDATE_PACKAGE "luci-app-lucky" "sirpdboy/luci-app-lucky" "main"
 #更新软件包版本
