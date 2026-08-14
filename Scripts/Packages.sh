8970271eabd5605a0de507fe9c45ebc04cd6775e
soda1107 <b5282521@live.cn>
2026-08-15 07:58:47 +0800

    add bandix

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -83,6 +83,7 @@ UPDATE_PACKAGE "luci-app-daed" "QiuSimons/luci-app-daed" "kix"
 UPDATE_PACKAGE "luci-app-dae" "QiuSimons/luci-app-dae" "kix"
 UPDATE_PACKAGE "luci-app-pushbot" "zzsj0928/luci-app-pushbot" "master"
 UPDATE_PACKAGE "luci-app-lucky" "sirpdboy/luci-app-lucky" "main"
+UPDATE_PACKAGE "luci-app-bandix" "timsaya/luci-app-bandix" "main"
 #更新软件包版本
 UPDATE_VERSION() {
 	local PKG_NAME=$1
