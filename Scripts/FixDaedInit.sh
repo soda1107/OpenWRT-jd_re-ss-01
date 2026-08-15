040aa3e2d5dc47f12211c4a77dd1fec25459bc54
soda1107 <b5282521@live.cn>
2026-08-15 16:22:25 +0800

    update

--- Scripts/FixDaedInit.sh
+++ Scripts/FixDaedInit.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-echo "======================================"
-echo " Fix daed init and NSS ECM IPv6 temp stop"
-echo "======================================"
 
 ########################################
 # Fix daed init
@@ -39,55 +36,6 @@ echo "[Daed] After:"
 grep -E "^START=|hijack_resolv_conf|restore_resolv_conf" "$TARGET" || true
 
 
-########################################
-# NSS ECM IPv6 temp stop
-########################################
 
-echo
-echo "===== Enable IPv6 ECM temp stop ====="
-
-
-ECM_SEARCH="$GITHUB_WORKSPACE/wrt"
-
-echo "[ECM] Searching source..."
-
-FILE=$(grep -rl \
-"ecm_front_end_ipv6_stopped" \
-"$ECM_SEARCH" \
-2>/dev/null | grep "qca-nss-ecm" | head -n 1 || true)
-
-
-if [ -z "$FILE" ]; then
-
-    echo "[ECM] ERROR: ECM source not found!"
-
-    grep -R \
-    "ecm_front_end_ipv6_stopped" \
-    "$ECM_SEARCH" \
-    2>/dev/null || true
-
-    exit 1
-
-fi
-
-
-echo "[ECM] Found:"
-echo "$FILE"
-
-
-echo "[ECM] Before:"
-grep -n \
--e "ecm_front_end_ipv6_stopped" \
--e "ecm_front_end_ipv6_stopped_temp" \
-"$FILE" || true
-
-
-########################################
-# 1. Keep permanent IPv6 ECM enabled
-########################################
-
-sed -i \
-'s/ecm_front_end_ipv6_stopped = 1/ecm_front_end_ipv6_stopped = 0/g' \
-"$FILE"
 
 
